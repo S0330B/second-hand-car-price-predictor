19f526a62cf1efd9edbad0bb8de509a24ad1f58f
S0330B <sushilsharmasubedi@gmail.com>
2025-10-18 13:50:45 +0545

    Update :: Update the model

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ pipe = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
 
 st.title('Car Price Prediction App')
 
-name = st.text_input("Enter the car name")
-company = st.text_input("Enter the car company")
+name = st.text_input("Enter the car name", value="Ford EcoSport Titanium")
+company = st.text_input("Enter the car company",value="Ford")
 year = st.number_input("Enter the car year", min_value=1900, max_value=2100, step=1)
 kms_driven = st.number_input("Enter the km driven", min_value=0, step=1)
-fuel_type = st.text_input("Enter the fuel type")
+fuel_type = st.text_input("Enter the fuel type",value="Diesel")
 fuel_type = fuel_type.strip().title()
 
 if st.button('Predict'):
@@ -21,4 +21,4 @@ if st.button('Predict'):
 
     prediction = pipe.predict(input_df)
 
-    st.success('The car price is: {}'.format(math.ceil(prediction[0])))
+    st.success('The car price is: {}'.format(math.ceil(prediction[0]*1.6)))
