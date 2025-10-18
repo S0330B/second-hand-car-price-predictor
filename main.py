44151d53955a2f12c2a7ea0022bbad25f94587dc
S0330B <sushilsharmasubedi@gmail.com>
2025-10-18 14:00:04 +0545

    Update :: Update the model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import math
+from datetime import datetime
 
 
 pipe = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
@@ -10,15 +11,22 @@ st.title('Car Price Prediction App')
 
 name = st.text_input("Enter the car name", value="Ford EcoSport Titanium")
 company = st.text_input("Enter the car company",value="Ford")
-year = st.number_input("Enter the car year", min_value=1900, max_value=2100, step=1)
+year = st.number_input("Enter the car year", min_value=1900, step=1)
 kms_driven = st.number_input("Enter the km driven", min_value=0, step=1)
 fuel_type = st.text_input("Enter the fuel type",value="Diesel")
 fuel_type = fuel_type.strip().title()
 
 if st.button('Predict'):
-    input_df = pd.DataFrame([[name, company, int(year), int(kms_driven), fuel_type]],
-                            columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'])
+    try:
+        if year <= datetime.now().year:
+            input_df = pd.DataFrame(
+                [[name, company, int(year), int(kms_driven), fuel_type]],
+                columns=['name', 'company', 'year', 'kms_driven', 'fuel_type']
+            )
+            prediction = pipe.predict(input_df)
+            st.success('The car price is: {}'.format(math.ceil(prediction[0] * 1.6)))
+        else:
+            st.error("⚠️ Year cannot be in the future.")
+    except Exception as e:
+        st.error(f"⚠️ Error: {e}")
 
-    prediction = pipe.predict(input_df)
-
-    st.success('The car price is: {}'.format(math.ceil(prediction[0]*1.6)))
